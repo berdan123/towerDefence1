1f760eb737e760239650f85366b5c5d1c18621a4
berdan123 <b_colak06@hotmail.com>
2025-01-15 22:05:01 +0000

    Update townManagement.py

--- townManagement.py
+++ townManagement.py
@@ -1,5 +1,52 @@
 import pygame
-import time
+class Player():
+    def __init__(self):
+        #attributes
+        self.gold=50 #amount of gold player has
+        self.elixir=50 #amount of elixir player has
+        self.maxGold=100 #maximum gold player can have
+        self.maxElixir=100 #maximum elixir player can have
+        self.goldPerTime=10 #the rate of gold increase
+        self.elixirPerTime=10 #rate of elixir increase
+        self.lastGoldUpdate= pygame.time.get_ticks() #last gold  increase time
+        self.lastElixirUpdate= pygame.time.get_ticks() #last  elixir increase time
+
+    def drawCurrency(self,screen,font):
+        goldText = font.render(f"Gold: {self.gold}/{self.maxGold}", True, (0, 0, 0)) #gold text
+        elixirText = font.render(f"Elixir: {self.elixir}/{self.maxElixir}", True, (0, 0, 0)) #elixir text
+        screen.blit(goldText, (10, 10))
+        screen.blit(elixirText, (200, 10))
+
+    def addGold(self,goldMineNum):
+        currentTime=pygame.time.get_ticks() #taking the current time
+        goldPerCycle=goldMineNum*self.goldPerTime #working out the amount of gold per cycle
+        if currentTime-self.lastGoldUpdate>= 30000: #checking if 30 seconds have passed
+            if self.gold<self.maxGold: #checking if the player is at max gold
+                self.gold+=goldPerCycle #increasaing gold
+            else: #if player is at max gold or more
+                self.gold=self.maxGold     #making sure player can not go over max gold
+            self.lastGoldUpdate=pygame.time.get_ticks() #updating the timer variable
+    def addElixir(self,labNum):
+        currentTime=pygame.time.get_ticks() #taking current time
+        elixirPerCycle = labNum * self.elixirPerTime #working out the amount of elixir per cycle
+
+        if currentTime-self.lastElixirUpdate>=30000: #checking if 30 seconds have passed
+            if self.elixir<self.maxElixir: #checking if player is at max elixir
+                self.elixir+=elixirPerCycle #increasing the elixir
+            else: #if player is at max elixir
+                self.elixir=self.maxElixir #ensuring player can not go past max elixir
+            self.lastElixirUpdate=pygame.time.get_ticks() #updating timer
+
+    #method to increase storage
+    def increaseGoldStorage(self):
+        self.maxGold+=50 #add 50 to max gold
+    def increaseElixirStorage(self):
+        self.maxElixir+=50 #add 50 to max elixir
+
+
+
+
+
 
 
 
@@ -52,6 +99,33 @@ def checkCollision(newRect,placedBuildings):
         if newRect.colliderect(rect):
             return True
     return False
+
+
+#back to tower defence battle
+def drawBattleButton(screen,font):
+    #creating button
+    towerButton=pygame.draw.rect(screen,(0,0,255),[0,560,90,30],0,5)
+    towerButtonText=font.render('Battle',True,(0,0,0)) #adding text
+    screen.blit(towerButtonText,(0,560)) #drawing text onto button
+    return towerButton #returning position of button
+
+def buildingUpgradeMenu(screen,font):
+    overlay = pygame.Surface((255, 255))  # size of the menu box
+    overlay.fill((255, 255, 255))  # white background
+    screen.blit(overlay, (300, 200))  # positioning at centre of screen
+
+    upgradeButton = pygame.draw.rect(screen, (255, 0, 0), (320, 210, 190, 40))  #
+    upgradeButtonText = font.render("Upgrade", True, (0, 0, 0))
+    screen.blit(upgradeButtonText, (330, 210))
+
+    # wgold storage  button
+    quitButton = pygame.draw.rect(screen, (0, 0, 255), (320, 260, 190, 40))
+    quitButtonText = font.render("Quit", True, (0, 0, 0))
+    screen.blit(quitButtonText, (330, 260))
+    pygame.display.update()
+    return {"upgrade": upgradeButton, "quit": quitButton}
+
+
 
 
 
